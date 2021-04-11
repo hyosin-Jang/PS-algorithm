f2393a8e6b184ea9116a088d1140fb0957e9c9cc
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-04-11 23:04:10 +0900

    정렬 알고리즘

---
+++ 자료구조/bubble_sort.c
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <Windows.h>
+//#define SWAP(x, y, t)((t) = (x), (x) = (y), (y) = (t))
+//#define MAX_SIZE 12
+//
+//void bubble_sort(int list[], int n) {
+//	int temp;
+//	for (int i = n - 1; i > 0; i--) {
+//		for (int j = 0; j<i; j++)
+//			if (list[j] > list[j + 1])
+//				SWAP(list[j], list[j + 1], temp);
+//	}
+//}
+//void main() {
+//
+//	int list[MAX_SIZE] = { 5,3,8,1,2,7,6,7,10,13,15,16 };
+//
+//	for (int i = 0; i < MAX_SIZE; i++)
+//		printf("%d ", list[i]);
+//	printf("\n\n");
+//
+//	bubble_sort(list, MAX_SIZE);
+//
+//	for (int i = 0; i < MAX_SIZE; i++) {
+//		printf("%d ", list[i]);
+//	}
+//
+//
+//
+//}
