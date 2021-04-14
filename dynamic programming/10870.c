c12a758d792186b66eb1642a8bba559df5bd0760
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-04-14 23:51:24 +0900

    Update 10870.c
    피보나치수열

--- dynamic programming/10870.c
+++ dynamic programming/10870.c
@@ -1,1 +1,19 @@
-#include <stdil.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int fib(int n) {
+	if (n == 0) return 0;
+	else if (n == 1) return 1;
+	else return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+
+	int n, num=0;
+	scanf("%d",&n);
+
+	num = fib(n);
+	printf("%d",num);
+
+	return 0;
+}
