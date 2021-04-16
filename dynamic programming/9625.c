d6316d8c666cfc7e2af31daeac4b7e8e585fa01a
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-04-16 23:52:55 +0900

    Update 9625.c

--- dynamic programming/9625.c
+++ dynamic programming/9625.c
@@ -1,1 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int fib(int n) {
+	if (n == 2)
+		return 1;
+	else if (n == 3)
+		return 2;
+	else return fib(n - 1) + fib(n - 2);
+}
+
+int main() {
+
+	int n, a=1, result;
+	scanf("%d",&n);
+
+	if (n == 1)
+		printf("0 1");
+	else
+		printf("%d %d",fib(n-1) , fib(n));
+
+	return 0;
+}
