d6411030c798769c17c4f255fa7105d9a96f6a38
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-05-22 23:42:05 +0900

    Add files via upload

---
+++ 1603.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main() {
+//	int num=1, sec, min, hour, ans;
+//	// √  
+//	scanf("%d", &num);
+//
+//	hour = num / 3600;
+//	min = (num % 3600) / 60;
+//	sec = (num % 60);
+//
+//	ans = hour + min + sec;
+//	printf("%d", ans);
+//
+//}
