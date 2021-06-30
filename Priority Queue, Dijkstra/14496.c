b86d2504a34533d9979fbdd5fb1c9e0770070896
hyosin-Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-06-30 23:57:58 +0900

    Update 14496.c

--- Priority Queue, Dijkstra/14496.c
+++ Priority Queue, Dijkstra/14496.c
@@ -3,34 +3,52 @@
 #include <stdlib.h>
 #define min(x,y)((x) < (y) ? (x) : (y))
 #define SIZE 1000
-int a, b, N, M, r, c, cnt=0;
+int a, b, N, M, r, c, cnt=10001;
 int arr[SIZE][SIZE] = {0};
 
 int FindMinChangeNum(int a, int b) {
-	// a¿¡¼­ b·Î °¡°í ½ÍÀ½
-	int num=0;
-	if (arr[a][b] == 1) // °æ·Î°¡ Á¸ÀçÇÑ´Ù´Â °Í 
-		return 1;
+	// aì—ì„œ bë¡œ ê°€ê³  ì‹¶ìŒ
+	int num=1;
+	if (arr[a][b] == 1) // ê²½ë¡œê°€ ì¡´ì¬í•œë‹¤ëŠ” ê²ƒ 
+		return num;
+	if (arr[b][a] == 1)
+		return num;
 
 	else {
-		for (int i = 1; i <= N; i++) 
+		for (int i = 1; i <= N; i++) {
 			if (arr[a][i] == 1) {
 				num += FindMinChangeNum(i, b);
 				cnt = min(cnt, num);
+				printf("[%dí–‰ %dì—´] ì—ì„œ cnt: %d\n", a, i, cnt);
 			}
+			if (arr[b][i] == 1) {
+				num += FindMinChangeNum(i, a);
+				cnt = min(cnt, num);
+				printf("[%dí–‰ %dì—´] ì—ì„œ cnt: %d\n", a, i, cnt);
+			}
+		}
 	}
 
 	return 0;
 }
 
 int main() {
-	scanf("%d %d",&a, &b);
-	scanf("%d %d", &N, &M);
-	while (M--) {
+	scanf("%d %d", &a, &b); getchar();
+	scanf("%d %d", &N, &M); getchar();
+	for (int i = 0; i < M; i++) {
 		scanf("%d %d",&r, &c);
 		arr[r][c] = 1;
+		arr[c][r] = 1;
 		getchar();
 	}
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++)
+			printf("%d ", arr[i][j]);
+		printf("\n");
+	}
+
+	FindMinChangeNum(a, b);
+	printf("cnt: %d M: %d\n",cnt, M);
 	if (cnt <= M)
 		printf("%d", cnt);
 	else
