7a88c1953c049cec62113bf7b7093c09f7f44a6b
hyosin Jang <71035113+hyosin-Jang@users.noreply.github.com>
2021-08-04 01:11:03 +0900

    Add files via upload

---
+++ 1427.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	string N;
+	cin >> N;
+	sort(N.begin(), N.end(), greater<char>());
+	cout << N;
+}
