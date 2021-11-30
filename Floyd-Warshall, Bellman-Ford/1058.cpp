42861261e7bab3ca548c05f69d51514870cf9047
hyosin Jang <cdnnnl@gmail.com>
2021-12-01 00:52:15 +0900

    친구

--- Floyd-Warshall, Bellman-Ford/1058.cpp
+++ Floyd-Warshall, Bellman-Ford/1058.cpp
@@ -1,62 +1,61 @@
 #include <iostream>
-#include <vector>
+#include <string>
 #include <algorithm>
 using namespace std;
 
-const int MAX = 50;
-char arr[MAX][MAX];
-int N;
+const int INF = 987654321;
 
-// 플로이드 와샬로 수정하기
-vector<int> check(N);
-int friends, twoFriends, gross;
+const int MAX = 50;
+
+int N;
+int friendsList[MAX][MAX];
+
+
+void floyd() {
+	for (int k = 0; k < N; k++) 
+		for(int i =0 ; i< N ; i++)
+			for (int j = 0; j < N; j++) {
+				if (i == j || j == k || i == k)
+					continue;
+				else if (friendsList[i][j] > friendsList[i][k] + friendsList[k][j])
+					friendsList[i][j] = friendsList[i][k] + friendsList[k][j];
+			}
+
+}
 
 int main() {
+
 	cin >> N;
 
-	for (int i = 0; i < N; i++)
-		for (int j = 0; j < N; j++)
-			cin >> arr[i][j];
-	
 	for (int i = 0; i < N; i++) {
+		string tmp;
+		cin >> tmp;
 
-		friends = 0, twoFriends = 0, gross = 0;
-		check.clear();
-
-		// 직접 연결된 친구
-		for (int j = 0; j < N; j++)
-			if (arr[i][j] == 'Y') {
-				friends++;
-				check.push_back(j);
-			}
-		cout << "\n"<< i+1 << "번째 friends: " << friends << "\n";
-		
-		// 겹친구 확인
-		int idx, x;
-		for (int j = 0; j < N; j++) {
-			if (arr[i][j] == 'N') {
-				for (int v = 0; v < check.size(); v++) {
-					idx = check[v]; // 2
-
-					cout << "x: " << j << " idx: " << idx << "\n";
-					// x와 y가 연결되어 있으면
-					if (arr[j][idx] == 'Y') {
-						twoFriends++;
-						break;
-					}
-				}
-				cout << j + 1 << "번째 twofriends: " << twoFriends << "\n";
-			}
+		for (int j = 0; j < tmp.size(); j++) {
+			if (i == j)
+				friendsList[i][j] = 0;
+			else
+				friendsList[i][j] = tmp[j] == 'Y' ? 1 : INF;
 		}
-		cout << friends + twoFriends << "\n";
-		gross = max(gross, friends + twoFriends);
-		
 	}
 
-	cout << gross << endl;
+	floyd();
 
+	int result = 0;
+	for (int i = 0; i < N; i++) {
+		int cnt = 0;
+		for (int j = 0; j < N; j++) {
+			if (i == j)
+				continue;
+			else if (friendsList[i][j] <= 2)
+				cnt++;
+		}
+		result = max(result, cnt);
+	
+	
+	}
 
-
+	cout << result << endl;
 
 	return 0;
 }
