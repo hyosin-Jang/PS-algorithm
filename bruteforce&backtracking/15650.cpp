8efb333e3aaf6d857e4cbe9be2be80feb822cb5d
hyosin Jang <cdnnnl@gmail.com>
2022-01-11 00:52:44 +0900

    220111 N과 M(2)

---
+++ bruteforce&backtracking/15650.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+const int MAX = 9;
+
+// dfs로 조합 구하기
+int arr[MAX] = {0, };
+int visited[MAX] = { 0, };
+int N, M;
+
+void dfs(int num, int cnt) { // 현재 위치
+	if (cnt == M) {
+		for (int i = 0; i < M; i++)
+			cout << arr[i] << ' ';
+		cout << '\n';
+		return;	
+	}
+	// for문 한번씩 돌때마다 한줄씩 출력
+	for (int i = num; i <= N; i++) {
+		if (!visited[i]) {
+			visited[i] = true; // 방문했다고 표시하기
+			arr[cnt] = i;
+			dfs(i+1, cnt + 1); // cnt +1 까지 더 깊게 들어가자
+			visited[i] = false;
+		}
+	}
+}
+
+int main() {
+
+	cin >> N >> M;
+	dfs(1, 0);
+
+	return 0;
+}
