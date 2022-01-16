e2ed6f3458dca4df3abc6f5abf6e8d5950bb4f35
hyosin Jang <cdnnnl@gmail.com>
2022-01-17 00:25:40 +0900

    220116 미로 탐색

--- graph/2178.cpp
+++ graph/2178.cpp
@@ -3,38 +3,53 @@
 using namespace std;
 
 const int MAX = 100 + 1;
-queue <pair<int, int>> Q;
-int visited[MAX][MAX];
-int maze[MAX][MAX];
+queue <pair<pair<int, int>, int>> Q;
+int visited[MAX][MAX] = {0, };
+int maze[MAX][MAX] = {0, };
 int N, M;
 string s;
-int cnt = 0;
 
-// 상하좌우 만들어야 함
 int x[4] = { -1, 1, 0, 0 };
 int y[4] = { 0, 0, 1, -1 };
 
 // BFS 사용
 
 void BFS() {
-	int popX, popY;
+	int nowX, nowY, nextX, nextY, cnt;
 	// 현재 위치 큐에 입력
-	Q.push(make_pair(1, 1));
-	cnt++;
+	Q.push(make_pair(make_pair(0, 0), 1));
+
+	// 방문했다고 표시
+	visited[0][0] = 1;
 
 	while (!Q.empty()) {
 		
 		
 		// 하나 빼기 
-		popX = Q.front().first; // 1
-		popY = Q.front().second; // 1
-		if(popX == )
+		nowX = Q.front().first.first; // 1
+		nowY = Q.front().first.second; // 1
+		cnt = Q.front().second;
+		Q.pop();
+
+		// 정지 조건
+		if (nowX == N-1 && nowY == M-1) {
+			cout << cnt << "\n";
+			return;
+		}
+		cnt++;
+
+		for (int i = 0; i < 4; i++) {
+			
+			nextX = nowX + x[i]; // 다음 x 좌표
+			nextY = nowY + y[i]; // 다음 y 좌표
+			if (!visited[nextX][nextY] && maze[nextX][nextY]==1 && nextX >= 0 && nextX < N && nextY >= 0 && nextY < M) {
 	
-	
-			// 정지 조건
+				Q.push(make_pair(make_pair(nextX, nextY), cnt)); // 다음 위치 Q에 넣기
+				visited[nextX][nextY] = 1;
+			
+			}
+		}
 	}
-
-
 
 }
 int main() {
@@ -42,7 +57,7 @@ int main() {
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++) {
-		cin >> s; // s=110110
+		cin >> s; 
 		for (int j = 0; j < M; j++)
 			maze[i][j] = s[j]-'0';
 	}
