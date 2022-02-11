81e74442b7116758517c3eccc7fbeb18400d705d
hyosin Jang <cdnnnl@gmail.com>
2022-02-12 02:15:03 +0900

    220211 Flat Earth

--- 2022 SUAPC/22986.cpp
+++ 2022 SUAPC/22986.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 int T;
 long long N, K;
+
 int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	cin >> T;
 
 	while (T--) {
 		cin >> N >> K;
-		int result = 0;
-		int temp = K + 1;
 
-		while (temp--) {
-			result += (N--) * 4;
-		}
-
-		cout << result << endl;
+		if (N - K - 1 < 0)
+			cout << 2 * N * (N + 1) <<"\n";
+		else
+			cout << 2*N*(N+1) - 2*(N-K-1)*(N-K) << "\n";
 
 	}
 
