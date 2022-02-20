dd822dd73b3323a7e528e870e200a7ca5f2e0b9b
hyosin Jang <cdnnnl@gmail.com>
2022-02-21 03:06:24 +0900

    220221 달리기 - BFS (방문한 곳 또 방문)

--- 2022 SUAPC/16930.cpp
+++ 2022 SUAPC/16930.cpp
@@ -4,15 +4,14 @@
 using namespace std;
 
 int N, M, K;
-int arr[1005][1005];
-int x1, y1, x2, y2;
+char arr[1005][1005];
+int sx, sy, ex, ey;
 int visit[1005][1005];
-struct Node {
-	int x;
-	int y;
-	int cnt;
-};
 
+
+struct Node {
+	int x, y, cnt;
+};
 
 queue<Node> Q;
 
@@ -22,17 +21,18 @@ int dy[4] = { 1, -1, 0, 0 };
 // cnt: 이동한 횟수
 void BFS() {
 	
-	Q.push({ x1, y1, 0 });
+	Q.push({ sx, sy, 0 });
+
+	visit[sx][sy] = 0;
 
 	while (!Q.empty()) {
-		int x = Q.front().x;
-		int y = Q.front().y;
+		int x = Q.front().x, y = Q.front().y;
 		int cnt = Q.front().cnt;
 		Q.pop();
 
-		if (x == x2 && y == y2) {
+		if (x == ex && y == ey) {
 			cout << cnt << endl;
-			return;
+			exit(0);
 		}
 
 		for (int i = 0; i < 4; i++) {
@@ -40,12 +40,13 @@ void BFS() {
 				int nx = x + j * dx[i];
 				int ny = y + j * dy[i];
 
-				if (nx >= 0 && nx < N && ny >= 0 && ny < M && arr[nx][ny]==0) {
+				// cout << "nx: " << nx << " ny: " << ny << "\n";
+				if (nx<1 || nx>N || ny<1 || ny>M || arr[nx][ny]== '#') break;
 					if (visit[nx][ny] == 1e9) {
 						// 처음 방문
 						visit[nx][ny] = cnt + 1; // cnt+1 번만에 도착
 						Q.push({ nx, ny, cnt + 1 });
-
+						// cout << "nx: " << nx << " ny: " << ny << " cnt: " << cnt +1<< "\n";
 					}
 					// 이미 방문한 지점일 때 같은 가중치면 계속 가보기
 					else if (visit[nx][ny] == cnt + 1) {
@@ -54,33 +55,26 @@ void BFS() {
 					}
 					else break;
 				
-				}
-	
-			}	
-		
+				}	
 		}
-
 	}
 }
 
 int main() {
-	cin >> N >> M >> K;
-	// 가로, 세로, 최대 개수
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
-	int tmp;
+	cin >> N >> M >> K;
+
 	// 빈칸은 0, 벽은 1
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			cin >> tmp;
-			if (tmp == '.')
-				arr[i][j] = 0;
-			else
-				arr[i][j] = 1;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) {
+			cin >> arr[i][j];
 			visit[i][j] = 1e9; // 초기화
 		}
 	}
 
-	cin >> x1 >> y1 >> x2 >> y2;
+	cin >> sx >> sy >> ex >> ey;
 
 	BFS();
 
