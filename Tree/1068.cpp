a6fb3da556b2fdc09095a331d097458ea30a2bc2
hyosin Jang <cdnnnl@gmail.com>
2022-05-24 22:56:57 +0900

    220524 트리

--- Tree/1068.cpp
+++ Tree/1068.cpp
@@ -1,47 +1,41 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
+vector<vector<int>> tree; // 2차원 tree 벡터
 
-void deleteNode(int node, vector<vector<int>> &tree) { // node를 삭제함
-	for (int i = tree[node].size(); i >= 0; i--) {
-		deleteNode(tree[node][i], tree);
-		tree[node].pop_back();
-	}
+//주어진 정점을 지웠을 때의 리프 노드의 수
+int eraseLeafCnt(int node, int& erase_node) { 
+    if (node == erase_node) { // 현재 노드가 지울 노드라면 
+        return 0; // 더이상 탐색하지 않고 0 리턴
+    }
+    if (tree[node].empty() || (tree[node].size() == 1 && tree[node][0] == erase_node)) {
+        return 1; // 비어있거나 size가 1인데 지울 노드가 들어있다면 리프 노드이므로 1 리턴
+    }
+    int cnt = 0; // 초기화
+    for (int i = 0; i < tree[node].size(); i++) { // 모든 자식들에 대해 
+        cnt += eraseLeafCnt(tree[node][i], erase_node); // dfs 방식으로 탐색하기 
+    }
+    return cnt; // 총 리프 노드의 개수 리턴
 }
 
-
 int main() {
-	int n, node;
+    int n, par, root, erase_node; // 노드 개수, 입력 받을 때 사용하는 부모 노드, 루트, 지울 노드
 
-	vector<vector<int>> tree(n, vector<int>(n, 0));
-	cin >> n;
-	for (int i = 0; i < n; i++) { // 입력
-		cin >> node;
-		tree[i].push_back(node);
-	}
+    //입력
+    cin >> n; // n 입력 받기
+    tree.assign(n, vector<int>(0)); // tree 벡터 0으로 초기화하기
+    for (int i = 0; i < n; i++) { // n 만큼 반복하면서 par 입력받기
+        cin >> par; // i번째 노드의 부모 노드 입력 받기
+        if (par == -1) { // par가 -1이라면
+            root = i; // i는 루트 노드
+            continue; // 다음으로 넘어가기
+        }
+        tree[par].push_back(i); // 부모 벡터에 i(자식) 추가하기
+    }
+    cin >> erase_node; // 지울 노드 입력받기
 
-	cin >> node;
-	deleteNode(node, tree);
-	// 부모가 2인 애들을 다 지움
-	// 그럼 다시 부모가 a인 애를 지움
-
-	// Q. 리프 노드를 어떻게 알 수 있을까?
-
-	// 자식 개수가 0인 노드
-
-	// -1 0 0 1 1
-
-	// Q. 자식 개수가 여러 개 있을 수 있지?
-
-	// 0 1 2 3 4 5 6 7 8 9 10
-	//-1 0 0 1 1
-	//   
-	//   
-
-	// 자식이 없으면 리프 노드. 
-
-	// 2차원 벡터로 구현할 것 .
-
-	return 0;
+    cout << eraseLeafCnt(root, erase_node); // 연산 & 출력
+    return 0;
 }
