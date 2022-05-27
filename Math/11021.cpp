109eea24f8f48344c14eaab6f827da8126af1b30
hyosin Jang <cdnnnl@gmail.com>
2022-05-28 01:28:51 +0900

    220528 A + B - 7

---
+++ Math/11021.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t, n1, n2;
+    cin >>t;
+    for(int i =0 ; i<t ; i++){
+        cin >> n1 >> n2;
+        cout << "Case #" << i+1 << ": " << n1 + n2 << '\n';
+       
+    }
+    
+    
+    return 0;
+}
